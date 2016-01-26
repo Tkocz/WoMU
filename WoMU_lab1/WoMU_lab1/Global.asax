813ff0f66051d7f06dddbc83ad249ce54f7624f4
Martin Bergqvist <s141564@student.hb.se>
2016-01-26 11:55:14 +0100

    init

---
+++ WoMU_lab1/WoMU_lab1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WoMU_lab1.MvcApplication" Language="C#" %>
---
+++ WoMU_lab1/WoMU_lab1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WoMU_lab1.Startup))]
+namespace WoMU_lab1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            
+        }
+    }
+}
